c1107b831fecf966c17536ab465c68ceaa47d82e
Janne Blomqvist <janne.blomqvist@aalto.fi>
2016-09-19 14:28:23 +0300

    Switch to Flask

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-ssh-hostca-src
+ssh-hostca-srv
 ==============
 
 A simple daemon that listens on HTTP, and signs ssh public host keys
@@ -6,11 +6,28 @@ with a host certificate, then returns the signed certificate.
 
 A client can get a signed ssh host cert with something like::
 
-  curl -o /etc/ssh/ssh_host_ed25519_key-cert.pub -F file=@/etc/ssh/ssh_host_ed25519_key.pub http://sshca.example.org:8080
+  curl -o /etc/ssh/ssh_host_ed25519_key-cert.pub -F file=@/etc/ssh/ssh_host_ed25519_key.pub http://sshca.example.org:5000
 
 A ssh known_hosts line for the CA can be retrieved with a normal HTTP GET request, like::
 
-  curl http://sshca.example.org:8080 >> /etc/ssh/ssh_known_hosts
+  curl http://sshca.example.org:5000 >> /etc/ssh/ssh_known_hosts
+
+Running
+-------
+
+ssh-hostca-srv is built using `flask <http://flask.pocoo.org/>`_ . You can start it directly with::
+
+  python path/to/ssh-hostca-srv.py
+
+This launches the flask internal webserver on
+port 8000. Alternatively, you can deploy it like any other WSGI
+app. E.g. with gunicorn, something like::
+
+  gunicorn --pythonpath path/to/src ssh-hostca-srv:app
+
+The environment variable ``SSH_CAPATH`` should point to the path of
+the SSH host CA private key. It defaults to ``./hostca`` in the
+current directory.
 
 TODO
 ----
--- src/ssh-hostca-srv.py
+++ src/ssh-hostca-srv.py
@@ -27,7 +27,15 @@ from builtins import open
 from future import standard_library
 standard_library.install_aliases()
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask, request
+from flask import Response
+
+app = Flask(__name__)
+app.debug = True
+
+import os
+
+capath = os.getenv('SSH_CAPATH', './hostca')
 
 def sign(pubkeypath, hostname):
     """Sign a ssh host public key"""
@@ -41,64 +49,46 @@ def sign(pubkeypath, hostname):
     subprocess.check_call(['/usr/bin/ssh-keygen', '-s', capath, 
                            '-I', hostname, '-h',
                            '-n', principals, pubkeypath])
-    certpath = pubkeypath.rsplit(',', maxsplit=1)[0] + '-cert.pub'
+    certpath = pubkeypath.rsplit('.', 1)[0] + '-cert.pub'
     return certpath
 
-class S(BaseHTTPRequestHandler):
-    def _set_headers(self, length=None):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/octet-stream')
-        if length != None:
-            self.send_header('Content-Length', length)            
-        self.end_headers()
 
-    def do_GET(self):
-        """Return a ssh_known_hosts line with the CA public key id"""
-        capub = capath + '.pub'
-        with open(capub, "rb") as f:
-            ca = f.read()
-        resp = b'@cert-authority * ' + ca
-        self._set_headers(len(resp))
-        self.wfile.write(resp)
+def do_GET():
+    """Return a ssh_known_hosts line with the CA public key id"""
+    capub = capath + '.pub'
+    with open(capub, "rb") as f:
+        ca = f.read()
+    resp = b'@cert-authority * ' + ca
+    return Response(resp, mimetype='application/octet-stream')
 
-    def do_HEAD(self):
-        self._set_headers()
-        
-    def do_POST(self):
-        """Client POST's a ssh public host key, server signs it with host CA,
-returns signed certificate.
-        """
-        import tempfile
-        import os
-        import socket
-        hostname = socket.gethostbyaddr(self.client_address[0])[0]
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        t = tempfile.NamedTemporaryFile(delete=False)
-        t.write(post_data)
-        t.close()
-        certpath = sign(t.name, hostname)
-        os.unlink(t.name)
-        with open(certpath, 'rb') as f:
-            certcontent = f.read()
-        os.unlink(certpath)
-        self._set_headers(length=len(certcontent))
-        self.wfile.write(certcontent)
-        
-def run(server_class=HTTPServer, handler_class=S, port=8080):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print('Starting httpd...')
-    httpd.serve_forever()
+def do_POST():
+    """Client POST's a ssh public host key, server signs it with host CA,
+    returns signed certificate.
+    """
+    import tempfile
+    import os
+    import socket
+    hostname = socket.gethostbyaddr(request.remote_addr)[0]
+    pubkeyfile = request.files['file']
+    if not pubkeyfile:
+        return ""
+    t = tempfile.mkdtemp()
+    pubkeypath = os.path.join(t, 'key.pub')
+    pubkeyfile.save(pubkeypath)
+    certpath = sign(pubkeypath, hostname)
+    with open(certpath, 'rb') as f:
+        certcontent = f.read()
+    os.unlink(certpath)
+    os.unlink(pubkeypath)
+    os.rmdir(t)
+    return Response(certcontent, mimetype='application/octet-stream')
 
+@app.route('/', methods=['GET', 'POST'])
+def myapp():
+    if request.method == 'GET':
+        return do_GET()
+    else:
+        return do_POST()
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(description='Run a HTTP service for signing SSH host keys.')
-    parser.add_argument('-p', '--port', default=8080, type=int)
-    parser.add_argument('-c', '--capath', default='./hostca', 
-                        help='Path to the host CA private key file')
-    args = parser.parse_args()
-    global capath
-    capath = args.capath
-    run(port=args.port)
+    app.run(debug=True)
